df74133bef6678e34b93202b856a1dd6e96dc25d
xkevio <xkeviota@gmail.com>
2021-12-01 19:30:55 +0100

    Run cargo fmt

--- src/day1.rs
+++ src/day1.rs
@@ -3,7 +3,7 @@ use std::fs;
 // pub fn part1_better() -> usize {
 //     let sonar_measurements =
 //         fs::read_to_string("src/inputs/day1.txt").expect("Unable to open file");
-    
+
 //     let mut vec = Vec::<i32>::new();
 //     let measurements: Vec<&str> = sonar_measurements.lines().collect();
 
@@ -20,10 +20,13 @@ pub fn part1() -> u32 {
     let sonar_measurements =
         fs::read_to_string("src/inputs/day1.txt").expect("Unable to open file");
 
-    let measurements: Vec<u32> = sonar_measurements.lines().map(|n| n.parse::<u32>().unwrap()).collect();
+    let measurements: Vec<u32> = sonar_measurements
+        .lines()
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
 
     for i in 1..measurements.len() {
-        if measurements[i] > measurements[i-1] {
+        if measurements[i] > measurements[i - 1] {
             increases += 1;
         }
     }
@@ -36,7 +39,10 @@ pub fn part2() -> u32 {
     let sonar_measurements =
         fs::read_to_string("src/inputs/day1.txt").expect("Unable to open file");
 
-    let measurements: Vec<u32> = sonar_measurements.lines().map(|n| n.parse::<u32>().unwrap()).collect();
+    let measurements: Vec<u32> = sonar_measurements
+        .lines()
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
     let windows = measurements.windows(3).collect::<Vec<_>>();
 
     for i in 1..windows.len() {
