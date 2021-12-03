6e1a25b92339adf47588abfd8b9e5ee1cf8bf3cc
xkevio <xkeviota@gmail.com>
2021-12-03 18:11:31 +0100

    Improve day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,59 +1,44 @@
 const INPUT: &str = include_str!("inputs/day3.txt");
 
 pub fn part1() -> u32 {
-    let mut gamma_rate = String::new();
-    let mut epsilon_rate = String::new();
+    let mut gamma_rate = 0;
 
     for i in 0..12 {
-        let one_count = INPUT.lines().filter(|l| l[i..l.len()].starts_with("1")).count();
-        let zero_count = INPUT.lines().filter(|l| l[i..l.len()].starts_with("0")).count();
+        let (ones, zeros): (Vec<&str>, Vec<&str>) =
+            INPUT.lines().partition(|l| l[i..l.len()].starts_with('1'));
 
-        if one_count > zero_count {
-            gamma_rate += "1";
+        if ones.len() > zeros.len() {
+            gamma_rate += 2u32.pow(11 - i as u32) * 1;
         } else {
-            gamma_rate += "0";
+            gamma_rate += 0;
         }
     }
 
-    // "flip bits"
-    epsilon_rate = gamma_rate.as_str().chars().map(|c| if c == '1' { '0' } else { '1' }).collect();
-    u32::from_str_radix(gamma_rate.as_str(), 2).unwrap() * u32::from_str_radix(epsilon_rate.as_str(), 2).unwrap()
+    // "flip bits" / invert
+    let epsilon = 2u32.pow(12) - 1 - gamma_rate;
+    gamma_rate * epsilon
 }
 
 pub fn part2() -> u32 {
-    let mut o2_gen_rating = 0;
-    let mut co2_scrubber_rating = 0;
-
     let nums: Vec<&str> = INPUT.lines().collect();
 
-    o2_gen_rating = retain_ml(&nums, true);
-    co2_scrubber_rating = retain_ml(&nums, false);
-
-    println!("{}, {}", &o2_gen_rating, &co2_scrubber_rating);
+    let o2_gen_rating = reduce_to_rating(&nums, true);
+    let co2_scrubber_rating = reduce_to_rating(&nums, false);
 
     o2_gen_rating * co2_scrubber_rating
 }
 
-fn retain_ml(numbers: &Vec<&str>, most_common: bool) -> u32 {
+fn reduce_to_rating(numbers: &[&str], start_bit: bool) -> u32 {
     let mut i = 0;
-    let mut numbers_copy = numbers.clone();
+    let mut numbers_copy = numbers.to_owned();
 
     while numbers_copy.len() > 1 {
-        let mut one_count = 0;
-        let mut zero_count = 0;
+        let (ones, zeros): (Vec<&str>, Vec<&str>) = numbers_copy.iter().partition(|l| l[i..l.len()].starts_with('1'));
 
-        for n in &numbers_copy {
-            if n[i..n.len()].starts_with("1") {
-                one_count += 1;
-            } else {
-                zero_count += 1;
-            }
-        }
-
-        if one_count >= zero_count {
-            numbers_copy.retain(|n| n[i..n.len()].starts_with((most_common as u32).to_string().as_str()));  
+        if ones.len() >= zeros.len() {
+            numbers_copy.retain(|n| if start_bit { ones.contains(n) } else { zeros.contains(n) });
         } else {
-            numbers_copy.retain(|n| n[i..n.len()].starts_with((!most_common as u32).to_string().as_str()));  
+            numbers_copy.retain(|n| if start_bit { zeros.contains(n) } else { ones.contains(n) });
         }
 
         i += 1;
