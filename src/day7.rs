116788fcaf8cf00be0d33ba73976494638ff29cb
xkevio <xkeviota@gmail.com>
2021-12-07 10:11:27 +0100

    Add statistical crate for day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,32 +1,30 @@
-use std::cmp::{max, min};
-
 const INPUT: &str = include_str!("inputs/day7.txt");
 
-fn get_median(list: &[usize]) -> usize {
-    if list.is_empty() {
-        return 0;
-    }
+// fn get_median(list: &[usize]) -> usize {
+//     if list.is_empty() {
+//         return 0;
+//     }
 
-    let mid = list.len() / 2;
+//     let mid = list.len() / 2;
     
-    if list.len() % 2 == 1 {
-        return list[mid];
-    } else {
-        return (list[mid - 1] + list[mid]) / 2;
-    }
-}
+//     if list.len() % 2 == 1 {
+//         return list[mid];
+//     } else {
+//         return (list[mid - 1] + list[mid]) / 2;
+//     }
+// }
 
 pub fn part1() -> usize {
-    let mut coordinates: Vec<usize> = INPUT.split(",").map(|f| f.parse().unwrap()).collect();
+    let mut coordinates: Vec<i32> = INPUT.split(",").map(|f| f.parse().unwrap()).collect();
     coordinates.sort();
 
-    let pos = get_median(&coordinates);
-    coordinates.iter().map(|f| max(*f, pos) - min(*f, pos)).sum()
+    let pos = statistical::median(&coordinates);
+    coordinates.iter().map(|f| (f - pos).abs() as usize).sum()
 }
 
 pub fn part2() -> usize {
-    let coordinates: Vec<usize> = INPUT.split(",").map(|f| f.parse().unwrap()).collect();
-    let pos = f32::ceil((coordinates.iter().sum::<usize>() / coordinates.len()) as f32) as usize;
+    let coordinates: Vec<f64> = INPUT.split(",").map(|f| f.parse().unwrap()).collect();
+    let pos = f64::floor(statistical::mean(&coordinates));
 
-    coordinates.iter().map(|f| max(*f, pos) - min(*f, pos)).map(|f| (f * (f + 1)) / 2).sum()
+    coordinates.iter().map(|f| (f - pos).abs() as usize).map(|f| ((f * (f + 1)) / 2)).sum()
 }
