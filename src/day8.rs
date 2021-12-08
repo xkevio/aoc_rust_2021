5c03423a24b728d066335cd889bbf68231705cf3
xkevio <xkeviota@gmail.com>
2021-12-08 15:46:02 +0100

    Fully add day 8

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("inputs/day8.txt");
 
@@ -18,11 +18,13 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    let mut sum = String::new();
     let mut patterns = HashMap::<char, &str>::new();
+    let mut num_sum: usize = 0;
 
     for line in INPUT.lines() {
+        let mut sum = String::new();
         let (inputs, outputs) = line.split_once(" | ").unwrap();
+        
         for input in inputs.split_whitespace() {
             match input.len() {
                 2 => {
@@ -49,17 +51,28 @@ pub fn part2() -> usize {
                 5 => {
                     if patterns.get(&'1').unwrap().chars().all(|c| output.contains(c)) {
                         sum.push('3');
-                    } else if patterns.get(&'1').unwrap().chars().any(|c| output.contains(c)) {
-                        sum.push('2');
-                    } else {
+                    } else if patterns.get(&'4').unwrap().chars().filter(|c| output.contains(*c)).count() == 3 {
                         sum.push('5');
+                    } else {
+                        sum.push('2');
                     }
                 },
+                6 => {
+                    if patterns.get(&'4').unwrap().chars().all(|c| output.contains(c)) {
+                        sum.push('9');
+                    } else if patterns.get(&'1').unwrap().chars().all(|c| output.contains(c)) {
+                        sum.push('0');
+                    } else {
+                        sum.push('6');
+                    }
+                },
+                7 => sum.push('8'),
                 _ => (),
             }
         }
+        println!("{}", &sum);
+        num_sum += usize::from_str_radix(sum.as_str(), 10).unwrap();
     }
 
-    0
-    //usize::from_str_radix(sum.as_str(), 10).unwrap()
+    num_sum
 }
