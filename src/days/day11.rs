3676a7f14a36a0bbb1ba106f55df3fbcf2bb3c1d
xkevio <xkeviota@gmail.com>
2021-12-11 20:07:32 +0100

    11: More functional style iterators for testing

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -20,7 +20,7 @@ fn get_neighbors(energy_levels: &[Vec<u32>], i: usize, j: usize) -> Vec<(usize, 
         .flatten()
         .filter(|(dx, dy)| *dx != 0 || *dy != 0)
         .collect();
-        
+
     for (dx, dy) in dpos {
         let (x, y) = (dx + i as i32, dy + j as i32);
         if x >= 0 && x < energy_levels.len() as i32 && y >= 0 && y < energy_levels[0].len() as i32 {
@@ -51,16 +51,15 @@ fn flash_octopus(part1: bool) -> usize {
         if part1 && moment > 100 {
             break;
         }
-        // increase by 1
-        for x in &mut energy_levels {
-            for y in x {
-                *y += 1;
-            }
-        }
+
+        //increase by 1
+        energy_levels = energy_levels
+            .iter()
+            .map(|v| v.iter().map(|n| *n + 1).collect())
+            .collect();
 
         // increase neighbors
         let mut marked: Vec<(usize, usize)> = Vec::new();
-
         for i in 0..energy_levels.len() {
             for j in 0..energy_levels[0].len() {
                 if energy_levels[i][j] > 9 {
@@ -76,13 +75,10 @@ fn flash_octopus(part1: bool) -> usize {
             .sum::<usize>();
 
         // reset
-        for x in &mut energy_levels {
-            for y in x {
-                if *y > 9 {
-                    *y = 0;
-                }
-            }
-        }
+        energy_levels = energy_levels
+            .iter()
+            .map(|v| v.iter().map(|n| if *n > 9 { 0 } else { *n }).collect())
+            .collect();
 
         if !part1 && energy_levels.iter().all(|v| v.iter().all(|f| *f == 0)) {
             return moment;
