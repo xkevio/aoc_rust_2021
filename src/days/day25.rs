5bf91d89cd32d7e3c0c8306a73d617e27f079bcd
xkevio <xkeviota@gmail.com>
2021-12-25 16:15:34 +0100

    25: Simplify loop and remove enum

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -1,20 +1,13 @@
-use std::collections::HashMap;
-
 const INPUT: &str = include_str!("../inputs/day25.txt");
-
-enum Direction {
-    Right,
-    Down,
-}
 
 fn parse_input() -> Vec<Vec<char>> {
     INPUT.lines().map(|l| l.chars().collect()).collect()
 }
 
-fn get_move(grid: &[Vec<char>], i: usize, j: usize, dir: Direction) -> Option<(usize, usize)> {
-    match dir {
-        Direction::Right => {
-            if (j + 1) < grid[0].len() && grid[i][j + 1] == '.' {
+fn get_move(grid: &[Vec<char>], i: usize, j: usize, c: char) -> Option<(usize, usize)> {
+    match c {
+        '>' => {
+            if (j + 1) < grid[0].len() {
                 if grid[i][j + 1] == '.' {
                     Some((i, j + 1))
                 } else {
@@ -28,7 +21,7 @@ fn get_move(grid: &[Vec<char>], i: usize, j: usize, dir: Direction) -> Option<(u
                 }
             }
         },
-        Direction::Down => {
+        'v' => {
             if (i + 1) < grid.len() {
                 if grid[i + 1][j] == '.' {
                     Some((i + 1, j))
@@ -43,6 +36,7 @@ fn get_move(grid: &[Vec<char>], i: usize, j: usize, dir: Direction) -> Option<(u
                 }
             }
         },
+        _ => None,
     }
 }
 
@@ -51,51 +45,26 @@ pub fn part1() -> usize {
     let mut steps: usize = 0;
 
     for step in 1.. {
-        let mut moves: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
-        let mut no_right_moves = false;
-        let mut no_down_moves = false;
+        let mut moved = false;
+        
+        for c in ['>', 'v'] {
+            let mut next = grid.clone();
+            for i in 0..grid.len() {
+                for j in 0..grid[0].len() {
+                    if grid[i][j] == c {
+                        if let Some(p) = get_move(&grid, i, j, c) {
+                            next[i][j] = '.';
+                            next[p.0][p.1] = c;
 
-        for i in 0..grid.len() {
-            for j in 0..grid[0].len() {
-                if grid[i][j] == '>' {
-                    if let Some(p) = get_move(&grid, i, j, Direction::Right) {
-                        moves.insert((i, j), p);
+                            moved = true;
+                        }
                     }
                 }
             }
+            grid = next;
         }
-
-        for (from, to) in &moves {
-            grid[from.0][from.1] = '.';
-            grid[to.0][to.1] = '>';
-        }
-
-        if moves.is_empty() {
-            no_right_moves = true;
-        }
-
-        moves.clear();
-
-        for i in 0..grid.len() {
-            for j in 0..grid[0].len() {
-                if grid[i][j] == 'v' {
-                    if let Some(p) = get_move(&grid, i, j, Direction::Down) {
-                        moves.insert((i, j), p);
-                    }
-                }
-            }
-        }
-
-        for (from, to) in &moves {
-            grid[from.0][from.1] = '.';
-            grid[to.0][to.1] = 'v';
-        }
-
-        if moves.is_empty() {
-            no_down_moves = true;
-        }
-
-        if no_right_moves && no_down_moves {
+        
+        if !moved {
             steps = step;
             break;
         }
